1f544036a11d5aaf79ab430ddeb5f4f438e8adf2
ajroberts0417 <ajroberts0417@gmail.com>
2018-08-15 10:26:33 -0700

    changed functional tests to use Django LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+#make our test, inheriting from LiveServerTestCase (a django testing class)
+class NewVisitorTest(LiveServerTestCase):
 
     # set up is always called at the beginning of the test
     def setUp(self):
@@ -31,7 +32,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Andrew was born to learn about a cool new online to-do app.
         # He goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -68,6 +69,3 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, he ends his short (but meaningful) existence
 
         browser.quit()
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
