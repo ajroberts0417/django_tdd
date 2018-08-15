e3fca93c831334a59f1e062e97d2da852981e92f
ajroberts0417 <ajroberts0417@gmail.com>
2018-08-15 16:17:08 -0700

    added new url/view/template for creating new lists. Changes templates/views/tests to remove redundancies from home page

--- lists/views.py
+++ lists/views.py
@@ -6,16 +6,14 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-
-    if request.method == 'POST':
-        Item.objects.create(text = request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
-
-
     return render(request, 'lists/home.html')
 
 def view_list(request):
-
     return render(request, 'lists/list.html',
     {'items': Item.objects.all()
     })
+
+# Should only ever get called for a POST request to make a new list item
+def new_list(request):
+    Item.objects.create(text = request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
