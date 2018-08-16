ea4361b899820868a9df4d601739f4aa5e2bf006
ajroberts0417 <ajroberts0417@gmail.com>
2018-08-16 16:36:49 -0700

    hack Functional Tests to check against a staging server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+import os
 import time
 
 MAX_WAIT = 5
@@ -13,6 +14,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         ## browser is an object owned by the NewVisitorTest class
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://'+staging_server
 
     ## tearDown is always called at the end, even if the test fails
     def tearDown(self):
